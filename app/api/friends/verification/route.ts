6aa0d0121e049144cd19beb61ecb344e5f4e5c86
me974974 <deidaradev@gmail.com>
2023-09-25 22:25:06 +0200

    ad

--- app/api/friends/verification/route.ts
+++ app/api/friends/verification/route.ts
@@ -38,6 +38,8 @@ export async function GET(
                 },
             });
 
+        console.log('amis', user?.friends);
+
         if (user && user.friends.length > 0) {
             NextResponse.json(true);
         } else {
