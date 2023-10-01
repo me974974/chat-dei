778d96d31a77e9e74796a6c9eea878d9a832c9f4
me974974 <deidaradev@gmail.com>
2023-10-01 14:34:31 +0200

    fix useEffect

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -3,13 +3,12 @@
 import axios from "axios";
 import { User } from "@prisma/client";
 import { useRouter } from "next/navigation";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import Avatar from "@/app/components/Avatar";
 import LoadingModal from "@/app/components/LoadingModal";
 import toast from "react-hot-toast";
 import getAreFriends from "@/app/actions/getAreFriends";
-// import getAreFriends from "@/app/actions/getAreFriends";
 
 interface UserBoxProps {
     data: User
@@ -22,17 +21,19 @@ const UserBox: React.FC<UserBoxProps> = ({
     const [isLoading, setIsLoading] = useState(false);
     const [areFriends, setArefriends] = useState<boolean | null>(null);
 
-    const handleCheckSpecialFriend = async () => {
-        try {
-            const result = await getAreFriends(data.id);
-            setArefriends(result);
-        } catch (error) {
-            console.error('Une erreur s\'est produite lors de la vérification de l\'ami spécial :', error);
-            setArefriends(false);
-        }
-    };
+    useEffect(() => {
+        const handleCheckSpecialFriend = async () => {
+            try {
+                const result = await getAreFriends(data.id);
+                setArefriends(result);
+            } catch (error) {
+                console.error('Une erreur s\'est produite lors de la vérification de l\'ami spécial :', error);
+                setArefriends(false);
+            }
+        };
 
-    handleCheckSpecialFriend();
+        handleCheckSpecialFriend();
+    }, [data.id]);
 
     const handleCick = useCallback(() => {
         setIsLoading(true);
