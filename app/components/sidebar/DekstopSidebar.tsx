8bf8a5532745bf662cc3c3670c720ce6c4adc619
DeidaDev <85557757+me974974@users.noreply.github.com>
2024-02-21 13:15:06 +0100

    Update DekstopSidebar.tsx

--- app/components/sidebar/DekstopSidebar.tsx
+++ app/components/sidebar/DekstopSidebar.tsx
@@ -38,7 +38,6 @@ const DekstopSidebar: React.FC<DekstopSidebarProps> = ({
                     lg:w-20
                     xl:px-6
                     lg:overflow-y-auto
-                    lg:bg-white
                     lg:bg-[#fe0000]
                     lg:border-r-[1px]
                     lg:pb-4
