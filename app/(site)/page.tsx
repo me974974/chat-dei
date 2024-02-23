c32484f7933412c6ebd708210613533bb1f77210
DeidaDev <85557757+me974974@users.noreply.github.com>
2024-02-23 16:52:52 +0100

    Update page.tsx

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
             py-12
             sm:px-6
             lg:px-8
-            bg-gray-100
+            bg-[#818181]
         "
       >
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
